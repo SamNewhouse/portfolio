5e7342f81cdc2b076dbc80ead2b47eb00c040f55
Sam Newhouse <hello@samnewhouse.co.uk>
2024-07-23 21:34:28 +0100

    added public email address

--- src/presentation/3-organisms/Contact.tsx
+++ src/presentation/3-organisms/Contact.tsx
@@ -18,7 +18,7 @@ const Contact: FC<Props> = ({ className }) => {
           </div>
           <div className="flex flex-col gap-2 min-[400px]:flex-row justify-center">
             <Link
-              href="#"
+              href="mailto:hello@samnewhouse.co.uk"
               className="inline-flex h-10 items-center justify-center rounded-md bg-primary px-8 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
               prefetch={false}
               target=""
