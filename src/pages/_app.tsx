4de7c70f77c6c797ec00339cdb66c8540b7b6f60
Sam Newhouse <60176563+SamNewhouse@users.noreply.github.com>
2025-03-06 18:14:07 +0000

    1.4.0 (#6)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
 import { Inter } from "next/font/google";
 import "../styles/globals.css";
@@ -11,6 +12,7 @@ const inter = Inter({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={`${inter.variable} font-sans`}>
+      <Analytics />
       <Component {...pageProps} />
     </main>
   );
